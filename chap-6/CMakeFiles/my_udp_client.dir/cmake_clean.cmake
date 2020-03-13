f76d3ff79f1e3e0df66ffab7e4bea2b751370f8f
sanjay <sanjayzzzhong@gmail.com>
2020-03-14 00:12:56 +0800

    untracked files on master: eadc711 update from desktop

---
+++ chap-6/CMakeFiles/my_udp_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_udp_client.dir/my_udp_client.c.o"
+  "my_udp_client"
+  "my_udp_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_udp_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chap-6/CMakeFiles/my_udp_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_udp_server.dir/my_udp_server.c.o"
+  "my_udp_server"
+  "my_udp_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_udp_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
